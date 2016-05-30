0473f54e6a5ee7ada38aefb54efd34d79826a0c1
Daniel Freire <danielfr3@me.com>
2016-05-30 16:07:40 -0300

    Core Data entity Recipe created.

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  recipez-app
+//
+//  Created by Daniel Freire on 30/05/16.
+//  Copyright © 2016 Daniel Freire. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+    
+    
+
+}
